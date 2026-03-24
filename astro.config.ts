72c07109008b2720d0fc72ce427aa5acdc5b5fde
naamahrgl <naama.hargil@gmail.com>
2026-03-24 18:48:33 +0200

    initial clean commit

---
+++ astro.config.ts
@@ -0,0 +1,31 @@
+import netlify from '@astrojs/netlify';
+import solidJs from '@astrojs/solid-js';
+import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
+import { defineConfig, envField } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [tailwind({ applyBaseStyles: false }), icon(), solidJs()],
+	// Update to your storefront URL
+	site: 'https://shop.astro.build',
+	output: 'server',
+	adapter: netlify({ imageCDN: true }),
+	vite: {
+		build: {
+			assetsInlineLimit(filePath) {
+				return filePath.endsWith('css');
+			},
+		},
+	},
+	image: {
+		// Update to your own image domains
+		domains: [
+			'localhost',
+			'shop-next.astro.build',
+			'shop.astro.build',
+			'main--astro-swag-shop.netlify.app',
+		],
+	},
+
+});
---
+++ tailwind.config.ts
@@ -0,0 +1,75 @@
+import kobalte from '@kobalte/tailwindcss';
+import typography from '@tailwindcss/typography';
+import type { Config } from 'tailwindcss';
+import animate from 'tailwindcss-animate';
+import colors from 'tailwindcss/colors.js';
+import { fontFamily } from 'tailwindcss/defaultTheme.js';
+import plugin from 'tailwindcss/plugin.js';
+
+export default {
+	content: ['./src/**/*.{astro,js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+fontFamily: {
+  sans: ['nextexit-variable', 'sans-serif'],
+},
+			colors: {
+				theme: {
+					base: {
+						50: "#f0562b",
+						100: "white",
+												200: "#EAEAEA",
+												
+												400: "#EAEAEA",
+												300: "white",
+
+												500: "green",
+
+												600: "#083da1",
+												700: "purple",
+												800: "purple",
+																								900: "#f0562b",
+
+												950: "purple",
+
+					}
+				},
+			},
+		},
+	},
+	plugins: [
+
+
+		animate,
+		kobalte,
+		typography,
+		plugin(function customStyles(api) {
+			api.addUtilities({
+				'.grid-center': {
+					display: 'grid',
+					'place-items': 'center',
+					'place-content': 'center',
+				},
+			});
+		}),
+	],
+	corePlugins: {
+		container: false,
+	},
+} satisfies Config;
+
+plugin(function ({ addUtilities }) {
+
+  addUtilities({
+
+    '.roun-0': {
+      fontVariationSettings: "'ROUN' 0",
+    },
+
+    '.roun-100': {
+      fontVariationSettings: "'ROUN' 100",
+    },
+
+  });
+
+});
